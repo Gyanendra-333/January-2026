0a47b558242a261b9d300aceb7d42a84a91aedb4
Gyanendra <pgyanendra333@gmail.com>
2026-01-06 17:51:28 +0530

    linked list

---
+++ 01 DSA with Python/Lect-03/LinkedList.py
@@ -0,0 +1,50 @@
+
+
+class Node:
+
+    def __init__(self, info, next=None):
+        self.info = info
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert_at_beginning(self, info):
+        new_node = Node(info, self.head)
+        self.head = new_node
+
+    def insert_at_end(self, info):
+        new_node = Node(info)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def delete_node(self, key):
+        current = self.head
+        previous = None
+
+        while current and current.info != key:
+            previous = current
+            current = current.next
+
+        if not current:
+            return  # Key not found
+
+        if previous:
+            previous.next = current.next
+        else:
+            self.head = current.next
+
+    def display(self):
+        current = self.head
+        elements = []
+        while current:
+            elements.append(current.info)
+            current = current.next
+        print(" -> ".join(map(str, elements)))
