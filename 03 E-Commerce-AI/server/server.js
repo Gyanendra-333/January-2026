8060a2320e98544ea4258362d5531a9e6d9869d3
Gyanendra <pgyanendra333@gmail.com>
2026-01-08 12:05:01 +0530

    setup postgressql database

--- 03 E-Commerce-AI/server/server.js
+++ 03 E-Commerce-AI/server/server.js
@@ -1,4 +1,13 @@
 import app from "./app.js";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
 
 const PORT = process.env.PORT || 4000;
 
