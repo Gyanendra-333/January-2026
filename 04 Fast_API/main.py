7a1370757629e98aa27c8b9771560c63f1f04f7d
Gyanendra <pgyanendra333@gmail.com>
2026-01-28 12:05:18 +0530

    fast api get

--- 04 Fast_API/main.py
+++ 04 Fast_API/main.py
@@ -1,4 +1,23 @@
-import fastapi
+from fastapi import FastAPI
 
-print("FastAPI module imported successfully.")
-print(fastapi.__version__)
+app = FastAPI(title="My First FastAPI Server")
+
+
+@app.get("/")
+async def root():
+    return {
+        "status": "running",
+        "message": "FastAPI server successfully started 🚀"
+    }
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+@app.get("/gyan")
+async def gyan():
+    return {
+        "message": "FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints."
+    }
