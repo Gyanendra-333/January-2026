35a28b2f54fa09ac24e2fd7689974824a4a53343
Gyanendra <pgyanendra333@gmail.com>
2026-01-30 17:37:59 +0530

    controller post

--- 05 Fast_API_CRUD/src/tasks/controller.py
+++ 05 Fast_API_CRUD/src/tasks/controller.py
@@ -1,4 +1,15 @@
+from src.tasks.dtos import TaskSchema
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModal
 
 
-def create_task():
-    return {"message": "Create Task Successful"}
+def create_task(body: TaskSchema, db: Session):
+
+    data = body.model_dump()
+    new_task = TaskModal(title=data["title"],
+                         description=data["description"],
+                         is_completed=data["is_completed"])
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return {"status": "task created successfully", "data": new_task}
--- 05 Fast_API_CRUD/src/tasks/router.py
+++ 05 Fast_API_CRUD/src/tasks/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.tasks import controller
+from src.tasks.dtos import TaskSchema
+from src.utils.db import get_db
 
 router = APIRouter(prefix="/tasks")
 
 
 @router.post("/create")
-def create_task_route():
-    return controller.create_task()
+def create_task_route(body: TaskSchema, db=Depends(get_db)):
+    return controller.create_task(body, db)
