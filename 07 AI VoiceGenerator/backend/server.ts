6fe4105973de677d7ef76f378f02184826c5dbb1
Gyanendra <pgyanendra333@gmail.com>
2026-02-09 18:22:58 +0530

    database

--- 07 AI VoiceGenerator/backend/server.ts
+++ 07 AI VoiceGenerator/backend/server.ts
@@ -2,14 +2,19 @@ import exprss from "express";
 import cors from "cors";
 import "dotenv/config";
 import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express'
 
 dotenv.config({ quiet: true });
 
 const app = exprss();
 const port = process.env.PORT || 3000;
 
+// Middleware 
 app.use(cors());
 app.use(exprss.json());
+app.use(exprss.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
+
 
 app.get("/", (req: any, res: any) => {
     res.send("Hello World! , Backend Server is Running");
