d10d3b611654fbe34c6c334bb6b55bf84b509bf1
Gyanendra <pgyanendra333@gmail.com>
2026-03-11 18:29:00 +0530

    basic index

--- 01 DSA with Python/JavaScript/index3.js
+++ 01 DSA with Python/JavaScript/index3.js
@@ -44,10 +44,64 @@
 // console.log(obj.a);
 
 
-for (var i = 0; i < 3; i++) {
-    setTimeout(() => {
-        console.log(i);
-    }, 0);
-}
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 0);
+// }
 
 
+// console.log(false && "Hello");
+
+// const [a, b] = [10, 20];
+
+// console.log(a);
+
+// let obj = {};
+
+// console.log(obj?.name);
+
+
+// let arr = [1, 2, 3];
+
+// let res = arr.map(x => x * 2);
+
+// console.log(res);
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let sum = arr.reduce((a, b) => a + b);
+
+// console.log(sum);
+
+
+
+// test();
+
+// function test() {
+//     console.log("Hello");
+// }
+
+
+// test();
+
+// var test = function () {
+//     console.log("Hello");
+// }
+
+
+
+// console.log({} == {});
+
+
+// console.log("10" - - "10");
+
+
+// console.log("5" + "5");
+
+
+// console.log(!"Hello");
+
+
+console.log(Array.isArray([]));
