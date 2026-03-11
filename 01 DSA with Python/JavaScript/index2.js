248620160cb53b82744fee8d29f9913e9260333a
Gyanendra <pgyanendra333@gmail.com>
2026-03-11 17:07:36 +0530

    basic code learning

--- 01 DSA with Python/JavaScript/index2.js
+++ 01 DSA with Python/JavaScript/index2.js
@@ -5,3 +5,33 @@ function test() {
 }
 
 console.log(typeof (3 + "gyan"));
+
+console.log("jhj" - 3);
+console.log("5" * 3);
+console.log("5" % 3)
+
+console.log(false + true);
+
+console.log(null + 1);
+
+console.log(undefined + 1);
+
+console.log([] == true);
+
+console.log(null === undefined);
+console.log(typeof undefined)
+console.log(typeof null)
+
+
+console.log([1, 2, 3878] + [4, 5, 6]);
+
+console.log([] + {});
+
+console.log({} + []);
+
+setTimeout(() => {
+    console.log("Hello");
+}, 0);
+Promise.resolve().then(() => {
+    console.log("Gyan");
+});
---
+++ 01 DSA with Python/JavaScript/index3.js
@@ -0,0 +1,53 @@
+
+
+
+// setTimeout(() => {
+//     console.log("Hello");//4
+// }, 0);
+
+// Promise.resolve().then(() => {
+//     console.log("Gyan");//3
+// },3000);
+
+
+
+// setTimeout(() => {
+//     console.log("timeout");
+// });
+
+// Promise.resolve().then(() => {
+//     console.log("promise");
+// });
+
+
+// function test() {
+//     console.log(a);
+// }
+
+// let a = 10;
+
+// test();
+
+// console.log(0.1 + 0.2 === 0.3);
+// console.log((0.1 + 0.2).toFixed(1) === "0.3");
+
+// let arr = [1, 2, 3];
+// let arr2 = [...arr, 4];
+
+// console.log(arr2); //[[1, 2, 3], 4]
+
+// let obj = { a: 1 };
+// let obj2 = obj;
+
+// obj2.a = 5;
+
+// console.log(obj.a);
+
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 0);
+}
+
+
